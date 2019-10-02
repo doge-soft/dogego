0ad6f5f01477473d06eedab0648adb7c62bf8098
Go Go Go <xiangrui@aiursoft.com>
2019-10-02 10:10:05 +0800

    add http server

--- api/servers/main.go
+++ api/servers/main.go
@@ -1,24 +1,26 @@
 package servers
 
 import (
+	"github.com/doge-soft/dogego_server_http"
 	"github.com/gin-gonic/gin"
 	"log"
 )
 
-var Servers []func (router *gin.Engine) error
+var Servers []func(router *gin.Engine) error
 
 // 注册所有的服务器
 func RegisterServers() {
-
+	// 注册HTTP服务器
+	RegisterServer(dogego_server_http.HTTPServerProtocol)
 }
 
-func RegisterServer(s func (router *gin.Engine) error) {
+func RegisterServer(s func(router *gin.Engine) error) {
 	Servers = append(Servers, s)
 }
 
 func StartServers(router *gin.Engine) {
 	for _, server := range Servers {
-		go func (serverCpy func (router *gin.Engine) error) {
+		go func(serverCpy func(router *gin.Engine) error) {
 			err := serverCpy(router)
 
 			if err != nil {
